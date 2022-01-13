e01ff817b1ae776dd4c0d72f9e754a6e36536e59
9r3w <97501247+9r3w@users.noreply.github.com>
2022-01-13 10:02:57 +0900

    update types fixed Blog.tsx s types

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import axios from "axios";
 
-export class Blog extends React.Component {
+interface MyState {
+  title: string;
+  body: string;
+}
+
+export class Blog extends React.Component<any, MyState> {
   constructor(props: any) {
     super(props);
 
     this.state = {
-      title: null,
-      body: null
+      title: "",
+      body: ""
     };
   }
   componentDidMount() {
     axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
       console.log(this.props.match);
-      const blog = res.data[parseInt(this.props.match.params.id) - 1];
+      const blog = res.data[parseInt(this.props.match.params.id, 10) - 1];
       //this.setState({ blog });
       this.setState({ title: blog.title, body: blog.body });
     });
